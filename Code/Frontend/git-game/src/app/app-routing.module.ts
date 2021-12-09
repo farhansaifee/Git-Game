00866dca7ec78df50eca813a6a9d7e3b3ee6a450
Mohammad Farhan Saifee <farhan.saifee11@gmail.com>
2021-12-09 15:04:44 +0100

    Created Homepage

--- Code/Frontend/git-game/src/app/app-routing.module.ts
+++ Code/Frontend/git-game/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'game', component:GameComponent},
+  {path:'aboutus', component:AboutusComponent},
+  {path:'contactus', component:ContactusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Code/Frontend/git-game/src/app/app.module.ts
+++ Code/Frontend/git-game/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
